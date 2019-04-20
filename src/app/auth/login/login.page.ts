dade4814e1d7cfd74cb98104d8fc0e0098139f02
Santiago de la Fuente <santiago.daniel.delafuente@hotmail.com>
2019-04-19 22:35:13 -0300

    Cuatro. Botones de login

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -14,28 +14,77 @@ export class LoginPage implements OnInit {
 
   email: string;
   password: string;
+  loading: boolean;
+  ingresar: boolean;
 
   constructor(
     private  router:  Router,
     public afAuth: AngularFireAuth
-  ) { }
+  ) {
+    this.loading = false;
+    // this.ingresar = false;
+
+  }
 
   ngOnInit() {
+    // this.loading = false;
+    // this.ingresar = false;
   }
 
   async login() {
-    // const = { this.email, this.password } = this;
+    this.loading = true;
     try {
+
       const response = await this.afAuth.auth.signInWithEmailAndPassword(this.email, this.password);
-      console.log(response);
+      // console.log(response);
       if (response.user) {
+        this.loading = false;
+        this.ingresar = false;
         this.router.navigateByUrl('bienvenido');
       }
 
     } catch (err) {
-        if (err.code == 'auth/user-not-found' ) {
-            console.dir(err);
-        }
+        this.loading = false;
+        this.ingresar = false;
+        // if (err.code === 'auth/user-not-found' ) {
+        //     console.dir(err);
+        // } else {
+        //   console.dir(err);
+        // }
+    }
+  }
+  verificar () {
+    if (this.email != undefined && this.password != undefined)
+      this.ingresar = true;
+    else
+      this.ingresar = false;
+  }
+
+  cargarUsuario(user) {
+    switch (user) {
+       case 'admin': {
+          this.email = 'admin@gmail.com';
+          this.password = '111111';
+          break;
+       }
+       case 'invitado': {
+         this.email = 'invitado@gmail.com';
+         this.password = '222222';
+          break;
+       }
+       case 'usuario': {
+         this.email = 'usuario@gmail.com';
+         this.password = '333333';
+          break;
+       }
+       case 'D': {
+          console.log('Poor');
+          break;
+       }
+       default: {
+          console.log('Invalid choice');
+          break;
+       }
     }
   }
 
